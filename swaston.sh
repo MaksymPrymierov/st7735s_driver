7d73d4ce49820b65bb45d9c665f9b74288a3e5d7
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-05-09 18:02:53 +0300

    Improve color mode selection
    Improved color mode selection and added arguments handling.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- swaston.sh
+++ swaston.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-read -p "background color: " bg_color
 
-read -p "swaston color: " color
+if [ $1 ]
+then
+	color_mode=$1
+else
+	echo "Choose color mode: "
+	echo "1 - classic swaston"
+	echo "2 - inverted swaston"
+	echo "3 - custom colorset"
+
+	read color_mode
+fi
+
+if [ $color_mode == 1 ]
+then
+	bg_color=0xffff
+	color=0x0000
+elif [ $color_mode == 2 ]
+then
+	bg_color=0x0000
+	color=0xffff
+else
+	read -p "background color: 0x" bg_color
+	bg_color="0x$bg_color"
+
+	read -p "swaston color: 0x" color
+	color="0x$color"
+fi
 
 echo $bg_color > /sys/class/st7735s/fill_screen
-
-
 
 echo "$color 4 20 17 51" > /sys/class/st7735s/draw_rect
 echo "$color 4 71 119 17" > /sys/class/st7735s/draw_rect
